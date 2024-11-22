fe7f06d92a201f821fd2e9fff38eb6aa56baf211
RoggerMen <130023813+RoggerMen@users.noreply.github.com>
2024-11-22 12:54:06 -0500

    Rama ClimaApp Terminada

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,154 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-import { Container } from '@mui/material'
+import { Box, Container, TextField, Typography } from '@mui/material'
+import { LoadingButton } from '@mui/lab';
+
+
+const API_WEATHER = `http://api.weatherapi.com/v1/current.json?key=${
+  import.meta.env.VITE_API_KEY
+}&q=`
+
+const initialWeather = {
+  city: "",
+  country: "",
+  temperature: 0,
+  condition: "",
+  conditionText: "",
+  icon: "",
+}
 
 
 function App() {
-  
+
+  const [city, setCity] = useState("");
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState({
+    error:false,
+    message:"",
+  });
+  const [weather, setWeather] = useState(initialWeather);
+
+  const onSubmit = async (e:React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("submit")
+    setError({error:false, message: ""});
+    setLoading(true)
+    try {
+      if(!city.trim()) throw {message: "El campo ciudad es obligatorio*"};
+      const response = await fetch(`${API_WEATHER}${city}`);
+      const data = await response.json();
+      if(data.error) throw {message: data.error.message};
+      console.log(data);
+
+      setWeather({
+        city: data.location.name,
+        country: data.location.country,
+        temperature: data.current.temp_c,
+        condition: data.current.condition.code,
+        conditionText: data.current.condition.text,
+        icon: data.current.condition.icon,
+      });
+
+    } catch (error) {
+      console.log(error);
+      setWeather(initialWeather);
+      setError({error:true, message: (error as {message:string}).message})
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  //console.log(import.meta.env.VITE_API_KEY);
+  //gutterBottom => es dejar un espacio en la parte inferior
   return (
     <>
 
-    <Container sx={{
-      mt: 8
+    <Container maxWidth="xs"
+    sx={{
+      mt: 2
     }}> 
+      <Typography
+      variant='h3'
+      component="h1"
+      align='center'
+      gutterBottom
+      >
+        Weather App
+      </Typography>
+      {error.error && (
+        <Typography color="error" sx={{m:1.5}}>
+          {error.message}
+        </Typography>
+      )}
+
+      <Box
+      sx={{
+        display:"grid", gap:2
+      }}
+      component="form"
+      autoComplete='off'
+      onSubmit={onSubmit}
+      >
+        <TextField 
+        id='city'
+        label="Ciudad"
+        variant='outlined'
+        size='small'
+        required
+        fullWidth
+        value={city}
+        onChange={(e) => setCity(e.target.value)}
+        error={error.error}
+        helperText={error.message}
+        />
+
+        <LoadingButton
+        type='submit'
+        variant='contained'
+        loading={loading}
+        loadingIndicator="Cargando..."
+        >
+          Buscar
+        </LoadingButton>
+      </Box>
+
+      {weather.city &&
+      <Box sx={{
+        mt:2,
+        display:"grid",
+        gap:2,
+        textAlign:"center"
+      }}
+      >
+        <Typography variant='h4'>
+          {weather.city} {weather.country}
+        </Typography>
+        <Typography variant='h5'>
+          {weather.temperature}°C
+        </Typography>
+        <Box> 
+        <img src={weather.icon} alt={weather.conditionText}  />
+        </Box>
+        <Typography variant='h6'>
+          {weather.conditionText}
+        </Typography>
+      </Box> 
+}
+      <Typography
+        textAlign="center"
+        sx={{ mt: 2, fontSize: "10px" }}
+      >
+        Powered by:{" "}
+        <a
+          href="https://www.weatherapi.com/"
+          title="Weather API"
+        >
+          WeatherAPI.com
+        </a>
+      </Typography>
+
     </Container>
+    
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,48 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import { createTheme, ThemeProvider, CssBaseline } from '@mui/material'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+import { CssBaseline } from "@mui/material";
 import { SnackbarProvider } from "notistack";
 
-
-const theme = createTheme({
-  palette: {
-    mode: 'light',
-    primary: {
-      main: '#004d40',
-      contrastText: '#ffffff',
-    },
-    secondary: {
-      main: '#f50057',
-      contrastText: '#ffffff',
-    },
-    common: {
-      white: '#ffffff',
-      black: '#000000',
-    },
-  },
-  
-})
-import { BrowserRouter } from 'react-router-dom'
-
-
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ThemeProvider theme={theme}>
-      <BrowserRouter>
-
-      <SnackbarProvider maxSnack={3} autoHideDuration={3000}>
-      <CssBaseline>
-        <App />
-      </CssBaseline>
-      </SnackbarProvider>
-      
-      </BrowserRouter>
-    </ThemeProvider>
-    
-  </StrictMode>,
-)
+    <SnackbarProvider>
+      <CssBaseline />
+      <App />
+    </SnackbarProvider>
+  </StrictMode>
+);
